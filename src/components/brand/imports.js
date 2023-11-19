b7a53b7abfd3d7ec6e5c0bb5dbfc779513abd94a
vt-code <dev.andreola@gmail.com>
2023-11-19 17:58:18 -0300

    Creating brand component

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assets/google.png";
+import gramercy from "../../assets/gramercy.png";
+import meta from "../../assets/meta.png";
+import subvrsive from "../../assets/subvrsive.png";
+import groove from "../../assets/groove.png";
+
+export { google, gramercy, meta, subvrsive, groove };
